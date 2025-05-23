fe6c8e9d7c98de455e9efaa64353160fc296b137
Esteban Toloza <estebantoloza1998@gmail.com>
2025-05-22 23:05:46 -0400

    pruebas

---
+++ server.js
@@ -0,0 +1,18 @@
+const PORT = process.env.PORT || 5000;
+
+const server = require('./src/app')({
+  logger: {
+    level: 'info',
+  },
+});
+
+const start = async () => {
+  try {
+    await server.listen({ host: '0.0.0.0', port: PORT });
+  } catch (error) {
+    server.log.error(error);
+    process.exit(1);
+  }
+};
+
+start();
